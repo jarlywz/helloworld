26fc5df45d3784563356db13afc5655f4c437193
Daiwenzhang <39355961+jarlywz@users.noreply.github.com>
2018-05-17 14:27:20 +0800

    再试一次
    哈哈哈哈~

--- globalvar.cpp
+++
@@ -1,5 +0,0 @@
-#include "globalvar.h"
-using namespace cv;
-
-Mat in_img = Mat();
-Mat out_img = Mat();
--- globalvar.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <opencv2\opencv.hpp>
-
-extern cv::Mat in_img;
-extern cv::Mat out_img;
